48225120a1294d456eb1749d232ea9e652e3d7b4
Kkozyriev <konstanty.kozyriew@gmail.com>
2024-01-04 19:58:42 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='APItest',
+      version='1.0',
+      description='Practice API testing',
+      author='Kostiantyn Kozyriev',
+      author_email='konstanty.kozyriew@gmail.com',
+      url='https://github.com/Kkozyriev',
+      packages=find_packages()
+      )
